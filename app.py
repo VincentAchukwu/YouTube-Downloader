44fffa2e947ca7ff19760411f5e4787aad172348
Vincent Achukwu <vincent.achukwu2@mail.dcu.ie>
2021-06-29 13:41:48 +0100

    tried implementing mp3

--- app.py
+++ app.py
@@ -4,8 +4,10 @@ import os, datetime
 
 app = Flask(__name__)
 
+# yt.streams.filter(progressive=True, file_extension="mp4").order_by("resolution")[-1].download()
+
 # initiating global string for download path
-vidDownload = ""
+fileDownload = ""
 
 @app.route("/")
 def index():
@@ -33,14 +35,26 @@ def result():
             vidLength = str(datetime.timedelta(seconds=ytd.length))
             vidRating = str("{:.1f}".format(ytd.rating))
             vidThumb = ytd.thumbnail_url
+
             # then using global variable for later use in /download route
-            global vidDownload
-            vidDownload = ytd.streams.get_highest_resolution().download()
-            
+            global fileDownload
+
+            # testing how to download mp3/mp4 files (Pytube doesn't seem to support mp3)
+            # if request.form["options"] == "mp3":
+            #     print("mp3 pressed")
+            #     fileType = "mp3"
+            #     fileDownload = ytd.streams.get_by_itag(251).download()
+            # if request.form["options"] == "mp4":
+            #     print("mp4 pressed")
+            #     fileType = "mp4"
+            #     fileDownload = ytd.streams.get_highest_resolution().download()
+
+            # but for now, we're just downloading via mp4
+            fileDownload = ytd.streams.get_highest_resolution().download()
             # then go back to root dir
             # os.chdir("../")
 
-            return render_template("result.html", msg=[vidTitle, vidViews, vidLength, vidRating, vidThumb, vidDownload])
+            return render_template("result.html", msg=[vidTitle, vidViews, vidLength, vidRating, vidThumb, fileDownload])
 
         except Exception as e:
             print("Invalid Video URL {}".format(e))
@@ -49,11 +63,11 @@ def result():
 
     # return render_template("index.html", msg="Invalid URL")
 
-# download the video via send_file and using global path (vidDownload)
+# download the video via send_file and using global path (fileDownload)
 @app.route("/download")
 def download_file():
-    global vidDownload
-    return send_file(vidDownload, as_attachment=True)
+    global fileDownload
+    return send_file(fileDownload, as_attachment=True)
 
 @app.route("/contactus", methods=["GET", "POST"])
 def contact():
@@ -65,8 +79,6 @@ def contact():
             redirect("/contactus")
 
     return render_template("contactus.html")
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
